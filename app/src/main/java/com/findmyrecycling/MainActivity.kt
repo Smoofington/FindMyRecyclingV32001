73009ae69e03b0ab31707aaa5e7961e30da412ac
Daniel Cullen <34354477+CullenDaniel@users.noreply.github.com>
2022-02-15 21:57:57 -0500

    Added Recyclable and location fields

--- app/src/main/java/com/findmyrecycling/MainActivity.kt
+++ app/src/main/java/com/findmyrecycling/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.findmyrecycling
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.findmyrecycling.ui.theme.FindMyRecyclingTheme
 
@@ -29,10 +34,32 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun RecycleSearch(name: String) {
+    var recyclable by remember{ mutableStateOf("")}
+    var location by remember{ mutableStateOf("")}
+
+    Column {
+        OutlinedTextField(
+            value = recyclable,
+            onValueChange = {recyclable = it},
+            label = { Text(stringResource(R.string.recyclable)) }
+        )
+
+
+        OutlinedTextField(
+            value = location,
+            onValueChange = { location = it },
+            label = { Text(stringResource(R.string.location)) }
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FindMyRecyclingTheme {
         Greeting("Android")
+        RecycleSearch("AAA")
     }
 }
--- app/src/main/java/com/findmyrecycling/ui/theme/Theme.kt
+++ app/src/main/java/com/findmyrecycling/ui/theme/Theme.kt
@@ -15,7 +15,9 @@ private val DarkColorPalette = darkColors(
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    background = RecyclingGreen,
+    surface = RecyclingGreen
 
     /* Other default colors to override
     background = Color.White,
