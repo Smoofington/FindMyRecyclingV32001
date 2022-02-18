6563cbdfd4d8d91c50f1ab14f164400487603d0d
Daniel Cullen <34354477+CullenDaniel@users.noreply.github.com>
2022-02-17 19:04:43 -0500

    Added The multiple location and search. Logic to add them will be created later

--- app/src/main/java/com/findmyrecycling/ProfileScreen.kt
+++ app/src/main/java/com/findmyrecycling/ProfileScreen.kt
@@ -31,7 +31,13 @@ class ProfileScreen : ComponentActivity() {
 fun ProfileOptions(name: String) {
     var profile by remember{ mutableStateOf("")}
     var mySavedLocations by remember{ mutableStateOf("")}
+    var mySavedLocation1 by remember{ mutableStateOf("")}
+    var mySavedLocation2 by remember{ mutableStateOf("")}
+    var mySavedLocation3 by remember{ mutableStateOf("")}
     var mySavedSearches by remember{ mutableStateOf("")}
+    var mySavedSearch1 by remember{ mutableStateOf("")}
+    var mySavedSearch2 by remember{ mutableStateOf("")}
+    var mySavedSearch3 by remember{ mutableStateOf("")}
 
     Column {
         OutlinedTextField(
@@ -41,7 +47,6 @@ fun ProfileOptions(name: String) {
             modifier = Modifier.fillMaxWidth()
         )
 
-
         OutlinedTextField(
             value = mySavedLocations,
             onValueChange = { mySavedLocations = it },
@@ -50,9 +55,51 @@ fun ProfileOptions(name: String) {
         )
 
         OutlinedTextField(
+            value = mySavedLocation1,
+            onValueChange = { mySavedLocation1 = it },
+            label = { Text(stringResource(R.string.mySavedLocation1)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = mySavedLocation2,
+            onValueChange = { mySavedLocation2 = it },
+            label = { Text(stringResource(R.string.mySavedLocation2)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = mySavedLocation3,
+            onValueChange = { mySavedLocation3 = it },
+            label = { Text(stringResource(R.string.mySavedLocation3)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
             value = mySavedSearches,
             onValueChange = { mySavedSearches = it },
             label = { Text(stringResource(R.string.mySavedSearches)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = mySavedSearch1,
+            onValueChange = { mySavedSearch1 = it },
+            label = { Text(stringResource(R.string.mySavedSearch1)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = mySavedSearch2,
+            onValueChange = { mySavedSearch2 = it },
+            label = { Text(stringResource(R.string.mySavedSearch2)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = mySavedSearch3,
+            onValueChange = { mySavedSearch3 = it },
+            label = { Text(stringResource(R.string.mySavedSearch3)) },
             modifier = Modifier.fillMaxWidth()
         )
 
