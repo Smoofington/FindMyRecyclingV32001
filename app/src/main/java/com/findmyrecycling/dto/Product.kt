cad3502f9c2d856572b960a9a43e59fc2e8052fb
pokhrebi <61396982+pokhrebi@users.noreply.github.com>
2022-02-20 15:12:18 -0500

    Update Product File

--- app/src/main/java/com/findmyrecycling/dto/Product.kt
+++ app/src/main/java/com/findmyrecycling/dto/Product.kt
@@ -2,7 +2,7 @@ package com.findmyrecycling.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Product(var product: String, var id : Int = 0, var common : String ) {
+data class Product(@SerializedName("product") var product: String, var id : Int = 0, var common : String ) {
     override fun toString(): String {
         return common
     }
