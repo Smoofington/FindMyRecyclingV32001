29df01debfac029dbe0c036016f6c362519d7b4f
SofwareDude <hamzarauf233@gmail.com>
2022-02-20 15:14:26 -0500

    Chnaged DAO from Class to interface Bug Fix.

--- app/src/main/java/com/findmyrecycling/dao/IProductDAO.kt
+++ app/src/main/java/com/findmyrecycling/dao/IProductDAO.kt
@@ -1,12 +1,10 @@
 package com.findmyrecycling.dao
 
-
 import com.findmyrecycling.dto.Product
 import retrofit2.Call
 import retrofit2.http.GET
 
-class IProductDAO {
+interface IProductDAO {
     @GET("/mMUesHYPkXjaFGfS/arcgis/rest/services/Recycling_Depot_Locations/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
     fun getAllProducts() : Call<ArrayList<Product>>
-
 }
