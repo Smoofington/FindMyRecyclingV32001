7ef940e1c40f52823be80238e983aff60f725e4a
SofwareDude <hamzarauf233@gmail.com>
2022-02-20 14:50:12 -0500

    Implementations added and updated with DAO.

--- app/src/main/java/com/findmyrecycling/dao/IProductDAO.kt
+++ app/src/main/java/com/findmyrecycling/dao/IProductDAO.kt
@@ -6,7 +6,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 class IProductDAO {
-    @GET()
+    @GET("/mMUesHYPkXjaFGfS/arcgis/rest/services/Recycling_Depot_Locations/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson")
     fun getAllProducts() : Call<ArrayList<Product>>
 
 }
--- app/src/main/java/com/findmyrecycling/dto/Product.kt
+++ app/src/main/java/com/findmyrecycling/dto/Product.kt
@@ -2,7 +2,7 @@ package com.findmyrecycling.dto
 
 import com.google.gson.annotations.SerializedName
 
-data class Product(@SerializedName("genus") var genus : String, var species : String, var common : String, var cultivar:String ="", var id : Int = 0 ) {
+data class Product(var product: String, var id : Int = 0, var common : String ) {
     override fun toString(): String {
         return common
     }
