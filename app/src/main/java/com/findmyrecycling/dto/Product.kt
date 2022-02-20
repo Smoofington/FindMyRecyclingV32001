343afbf73751c21e7383e6ff914d846f72071533
pokhrebi <61396982+pokhrebi@users.noreply.github.com>
2022-02-20 14:17:41 -0500

    Add DTO File

--- app/src/main/java/com/findmyrecycling/dto/Product.kt
+++ app/src/main/java/com/findmyrecycling/dto/Product.kt
@@ -1,4 +1,9 @@
 package com.findmyrecycling.dto
 
-class Product {
+import com.google.gson.annotations.SerializedName
+
+data class Product(@SerializedName("genus") var genus : String, var species : String, var common : String, var cultivar:String ="", var id : Int = 0 ) {
+    override fun toString(): String {
+        return common
+    }
 }
