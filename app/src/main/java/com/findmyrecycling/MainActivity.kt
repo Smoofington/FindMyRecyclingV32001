af61a35b58d42040f53887d2a37b9edb43b23078
Falconsc <75039653+Falcons1231@users.noreply.github.com>
2022-03-06 07:11:36 -0500

    Code Review Updates

--- app/src/main/java/com/findmyrecycling/MainActivity.kt
+++ app/src/main/java/com/findmyrecycling/MainActivity.kt
@@ -4,14 +4,19 @@ package com.findmyrecycling
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.findmyrecycling.ui.theme.FindMyRecyclingTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,19 +40,29 @@ fun RecycleSearch(name: String) {
     var location by remember{ mutableStateOf("")}
 
     Column {
-        OutlinedTextField(
+        TextField(
             value = recyclable,
             onValueChange = {recyclable = it},
             label = { Text(stringResource(R.string.recyclable)) },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                .border(width = 1.dp,
+                        color = Color.Black,
+                        shape = RoundedCornerShape(4.dp)),
         )
 
 
-        OutlinedTextField(
+        TextField(
             value = location,
             onValueChange = { location = it },
             label = { Text(stringResource(R.string.location)) },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                .border(width = 1.dp,
+                        color = Color.Black,
+                        shape = RoundedCornerShape(4.dp)),
         )
 
     }
--- app/src/main/java/com/findmyrecycling/ProfileScreen.kt
+++ app/src/main/java/com/findmyrecycling/ProfileScreen.kt
@@ -3,14 +3,19 @@ package com.findmyrecycling
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.findmyrecycling.ui.theme.FindMyRecyclingTheme
 
 class ProfileScreen : ComponentActivity() {
@@ -38,22 +43,37 @@ fun ProfileOptions(name: String) {
             value = profile,
             onValueChange = {profile = it},
             label = { Text(stringResource(R.string.profile)) },
-            modifier = Modifier.fillMaxWidth()
-        )
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                .border(width = 1.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(4.dp)),
 
+        )
 
         OutlinedTextField(
             value = mySavedLocations,
             onValueChange = { mySavedLocations = it },
             label = { Text(stringResource(R.string.mySavedLocations)) },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                .border(width = 1.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(4.dp)),
         )
 
         OutlinedTextField(
             value = mySavedSearches,
             onValueChange = { mySavedSearches = it },
             label = { Text(stringResource(R.string.mySavedSearches)) },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                .border(width = 1.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(4.dp)),
         )
 
     }
