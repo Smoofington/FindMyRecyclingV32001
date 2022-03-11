3dc27875e166c8723759cb461f6cb5c07b276879
SofwareDude <hamzarauf233@gmail.com>
2022-03-11 18:55:14 -0500

    Location DTO created (Please check)

--- app/src/main/java/com/findmyrecycling/dto/Location.kt
+++ app/src/main/java/com/findmyrecycling/dto/Location.kt
@@ -1,6 +1,6 @@
 package com.findmyrecycling.dto
 
-class Location (var id : Int = 0, var name : String, var product : String, var common: String){
+data class Location (var id : Int = 0, var name : String, var product : String, var common: String){
     override fun toString() : String {
         return common
     }
