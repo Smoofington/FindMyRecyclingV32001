69030d92b813a2597397a8d4bb7709f31a4a88ec
SofwareDude <hamzarauf233@gmail.com>
2022-03-22 19:27:00 -0400

    DTO name change to Facility

--- app/src/main/java/com/findmyrecycling/MainActivity.kt
+++ app/src/main/java/com/findmyrecycling/MainActivity.kt
@@ -4,6 +4,7 @@ package com.findmyrecycling
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,14 +12,19 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.window.PopupProperties
+import com.findmyrecycling.dto.Product
 import com.findmyrecycling.ui.theme.FindMyRecyclingTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel : MainViewModel by viewModel<MainViewModel>()
+    private val viewModel: MainViewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,43 +33,131 @@ class MainActivity : ComponentActivity() {
             val products by viewModel.products.observeAsState(initial = emptyList())
             FindMyRecyclingTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background,
-                modifier = Modifier.fillMaxWidth()) {
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
                     RecycleSearch("Android")
                 }
             }
         }
     }
-}
 
-@Composable
-fun RecycleSearch(name: String) {
-    var recyclable by remember{ mutableStateOf("")}
-    var location by remember{ mutableStateOf("")}
+    @Composable
+    fun TextFieldWithDropdownUsage(
+        dataIn: List<Product>,
+        label: String = "",
+        take: Int = 3,
+        selectedSpecimen: Specimen = Specimen()
+    ) {
 
-    Column {
-        OutlinedTextField(
-            value = recyclable,
-            onValueChange = {recyclable = it},
-            label = { Text(stringResource(R.string.recyclable)) },
-            modifier = Modifier.fillMaxWidth()
+        val dropDownOptions = remember { mutableStateOf(listOf<Product>()) }
+        val textFieldValue = remember(selectedSpecimen.specimenId) { mutableStateOf(TextFieldValue(selectedSpecimen.plantName)) }
+        val dropDownExpanded = remember { mutableStateOf(false) }
+
+        fun onDropdownDismissRequest() {
+            dropDownExpanded.value = false
+        }
+
+        fun onValueChanged(value: TextFieldValue) {
+            inProductName = value.text
+            dropDownExpanded.value = true
+            textFieldValue.value = value
+            dropDownOptions.value = dataIn.filter {
+                it.toString().startsWith(value.text) && it.toString() != value.text
+            }.take(take)
+        }
+
+        TextFieldWithDropdown(
+            modifier = Modifier.fillMaxWidth(),
+            value = textFieldValue.value,
+            setValue = ::onValueChanged,
+            onDismissRequest = ::onDropdownDismissRequest,
+            dropDownExpanded = dropDownExpanded.value,
+            list = dropDownOptions.value,
+            label = label
         )
-
-
-        OutlinedTextField(
-            value = location,
-            onValueChange = { location = it },
-            label = { Text(stringResource(R.string.location)) },
-            modifier = Modifier.fillMaxWidth()
-        )
-
     }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FindMyRecyclingTheme {
-        RecycleSearch("Android")
+    @Composable
+    fun TextFieldWithDropdown(
+        modifier: Modifier = Modifier,
+        value: TextFieldValue,
+        setValue: (TextFieldValue) -> Unit,
+        onDismissRequest: () -> Unit,
+        dropDownExpanded: Boolean,
+        list: List<Product>,
+        label: String = ""
+    ) {
+        Box(modifier) {
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusChanged { focusState ->
+                        if (!focusState.isFocused)
+                            onDismissRequest()
+                    },
+                value = value,
+                onValueChange = setValue,
+                label = { Text(label) },
+                colors = TextFieldDefaults.outlinedTextFieldColors()
+            )
+            DropdownMenu(
+                expanded = dropDownExpanded,
+                properties = PopupProperties(
+                    focusable = false,
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true
+                ),
+                onDismissRequest = onDismissRequest
+            ) {
+                list.forEach { text ->
+                    DropdownMenuItem(onClick = {
+                        setValue(
+                            TextFieldValue(
+                                text.toString(),
+                                TextRange(text.toString().length)
+                            )
+                        )
+                        selectedProduct = text
+                    }) {
+                        Text(text = text.toString())
+                    }
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun RecycleSearch(name: String) {
+        var recyclable by remember { mutableStateOf("") }
+        var location by remember { mutableStateOf("") }
+
+        Column {
+            OutlinedTextField(
+                value = recyclable,
+                onValueChange = { recyclable = it },
+                label = { Text(stringResource(R.string.recyclable)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+
+            OutlinedTextField(
+                value = location,
+                onValueChange = { location = it },
+                label = { Text(stringResource(R.string.location)) },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        FindMyRecyclingTheme {
+            RecycleSearch("Android")
+        }
     }
 }
--- app/src/main/java/com/findmyrecycling/dto/Facility.kt
+++ app/src/main/java/com/findmyrecycling/dto/Facility.kt
@@ -1,6 +1,6 @@
 package com.findmyrecycling.dto
 
-data class Location (var id : Int = 0, var name : String, var product : String){
+data class Facility (var id : Int = 0, var name : String, var product : String){
     override fun toString() : String {
         return name
     }
