b5290e81d58329e44cf797c14d4027aa6d15733c
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-04-04 08:57:41 -0400

    Optimized imports

--- app/src/main/java/com/findmyrecycling/ProfileScreen.kt
+++ app/src/main/java/com/findmyrecycling/ProfileScreen.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
