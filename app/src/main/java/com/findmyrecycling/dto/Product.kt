b41a76e4c7a9f57612d050ed72a6ed513159cc3e
Tran, Jimmy (tranjy) <tranjy@mail.uc.edu>
2022-04-04 09:10:10 -0400

    Edited indentation for Product data class

--- app/src/main/java/com/findmyrecycling/dto/Product.kt
+++ app/src/main/java/com/findmyrecycling/dto/Product.kt
@@ -4,8 +4,10 @@ import com.google.gson.annotations.SerializedName
 
 data class Product(
     var product : String,
-    var id : Int = 0,) {
-    override fun toString(): String {
-        return product
+    var id : Int = 0
+    )
+    {
+        override fun toString(): String {
+            return product
     }
 }
