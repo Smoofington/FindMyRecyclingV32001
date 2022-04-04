571d1ef5eee4ddf02e54c9ec79fe15a4ad603499
pokhrebi <pokhrebi@dev.azure.com>
2022-04-03 20:02:18 -0400

    Facility Parameters

--- app/src/main/java/com/findmyrecycling/dto/Facility.kt
+++ app/src/main/java/com/findmyrecycling/dto/Facility.kt
@@ -1,4 +1,10 @@
 package com.findmyrecycling.dto
 
-class Facility (var location: String = ""){
+class Facility (
+    var facilityId: int = 0,
+    var productId: int = 0,
+    var facilityName: String = "",
+    var facilityDescription: String = "",
+    var longitude: String = "",
+    var latitude: String = ""){
 }
