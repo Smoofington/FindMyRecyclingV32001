bd86339d275b6e3e9011d12b1131bd790005a87c
Daniel Cullen <34354477+CullenDaniel@users.noreply.github.com>
2022-04-07 00:28:16 -0400

    Made Secondary Page UI

--- app/src/main/java/com/findmyrecycling/ProfileScreen.kt
+++ app/src/main/java/com/findmyrecycling/ProfileScreen.kt
@@ -3,11 +3,12 @@ package com.findmyrecycling
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,34 +28,68 @@ class ProfileScreen : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun MainMenu(){
+    Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd) {
+        Icon(imageVector = Icons.Filled.Menu, contentDescription = "")
+
+    }
+}
+
 @Composable
 fun ProfileOptions(name: String) {
-    var profile by remember{ mutableStateOf("")}
-    var mySavedLocations by remember{ mutableStateOf("")}
-    var mySavedSearches by remember{ mutableStateOf("")}
+    var facilityName by remember{ mutableStateOf("")}
+    var facilityLocation by remember{ mutableStateOf("")}
+    var facilityDetails by remember{ mutableStateOf("")}
+    var recyclableProduct by remember{ mutableStateOf("")}
 
     Column {
         OutlinedTextField(
-            value = profile,
-            onValueChange = {profile = it},
-            label = { Text(stringResource(R.string.profile)) },
+            value = facilityName,
+            onValueChange = {facilityName = it},
+            label = { Text(stringResource(R.string.facilityName)) },
             modifier = Modifier.fillMaxWidth()
         )
 
 
         OutlinedTextField(
-            value = mySavedLocations,
-            onValueChange = { mySavedLocations = it },
-            label = { Text(stringResource(R.string.mySavedLocations)) },
+            value = facilityLocation,
+            onValueChange = { facilityLocation = it },
+            label = { Text(stringResource(R.string.facilityLocation)) },
             modifier = Modifier.fillMaxWidth()
         )
 
         OutlinedTextField(
-            value = mySavedSearches,
-            onValueChange = { mySavedSearches = it },
-            label = { Text(stringResource(R.string.mySavedSearches)) },
+            value = facilityDetails,
+            onValueChange = { facilityDetails = it },
+            label = { Text(stringResource(R.string.facilityDetails)) },
             modifier = Modifier.fillMaxWidth()
         )
+        OutlinedTextField(
+            value = recyclableProduct,
+            onValueChange = { recyclableProduct = it },
+            label = { Text(stringResource(R.string.recyclableProduct)) },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row {
+            Button(
+                onClick = {
+                    // Toast.makeText(context, "$name")
+                }
+            )
+            {
+                Text(text = "Save")
+            }
+            Button(
+                onClick = {
+                    // Toast.makeText(context, "$name")
+                }
+            )
+            {
+                Text(text = "Photo")
+            }
+        }
 
     }
 }
