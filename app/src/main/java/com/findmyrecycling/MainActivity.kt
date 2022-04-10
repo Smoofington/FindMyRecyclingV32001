7a8ead0ecceeb7d57b84020c9165558769bdaed8
SofwareDude <hamzarauf233@gmail.com>
2022-04-10 16:42:33 -0400

    MainViewModel and MainActivity modifications

--- app/src/main/java/com/findmyrecycling/MainActivity.kt
+++ app/src/main/java/com/findmyrecycling/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.findmyrecycling
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
 import android.media.Image
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.border
@@ -27,7 +34,10 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.core.content.res.ResourcesCompat
+import com.findmyrecycling.dto.Photo
 import com.findmyrecycling.dto.Product
 import com.findmyrecycling.ui.theme.FindMyRecyclingTheme
 import com.google.common.collect.Collections2.filter
@@ -41,8 +51,19 @@ import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 var selectedProduct: Product? = null
+private var uri: Uri? = null
+private lateinit var currentImagePath: String
+private var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+private val viewModel: MainViewModel by viewModel<MainViewModel>()
+private var inProducttName: String = ""
+private var strUri by mutableStateOf("")
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -179,25 +200,105 @@ class MainActivity : ComponentActivity() {
                             color = Color.Black,
                             shape = RoundedCornerShape(4.dp)),
             )
-            Button (
-                onClick = {
-                   // Toast.makeText(context, "$name")
+            Row(modifier = Modifier.padding(all = 2.dp)) {
+                Button(
+                    onClick = {
+                        // Toast.makeText(context, "$name")
+                    }
+                )
+                {
+                    Text(text = "Save")
                 }
-                    )
-            {
-                Text(text = "Save")
-            }
-            Button (
-                onClick = {
-                    addFacility()
+                Button(
+                    onClick = {
+                        takePhoto()
+                    }
+                )
+                {
+                    Text(text = "Photo")
                 }
-            )
-            {
-                Text(text = "Add Facility")
+                Button(
+                    onClick = {
+                        addFacility()
+                    }
+                )
+                {
+                    Text(text = "Add Facility")
+                }
             }
-
         }
     }
+
+    private fun takePhoto() {
+        if (hasCameraPermission() == PackageManager.PERMISSION_GRANTED && hasExternalStoragePermission() == PackageManager.PERMISSION_GRANTED) {
+            // The user has already granted permission for these activities.  Toggle the camera!
+            invokeCamera()
+        } else {
+            // The user has not granted permissions, so we must request.
+            requestMultiplePermissionsLauncher.launch(arrayOf(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ))
+        }
+    }
+
+    private val requestMultiplePermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()) {
+            resultsMap ->
+        var permissionGranted = false
+        resultsMap.forEach {
+            if (it.value == true) {
+                permissionGranted = it.value
+            } else {
+                permissionGranted = false
+                return@forEach
+            }
+        }
+        if (permissionGranted) {
+            invokeCamera()
+        } else {
+            Toast.makeText(this, "Unable to load camera without permission.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun invokeCamera() {
+        val file = createImageFile()
+        try {
+            uri = FileProvider.getUriForFile(this, "app.plantdiary.fileprovider", file)
+        } catch (e: Exception) {
+            Log.e(ContentValues.TAG, "Error: ${e.message}")
+            var foo = e.message
+        }
+        getCameraImage.launch(uri)
+    }
+
+    private fun createImageFile() : File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "Specimen_${timestamp}",
+            ".jpg",
+            imageDirectory
+        ).apply {
+            currentImagePath = absolutePath
+        }
+    }
+
+    private val getCameraImage = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            success ->
+        if (success) {
+            Log.i(ContentValues.TAG, "Image Location: $uri")
+            strUri = uri.toString()
+            val photo = Photo(localUri = uri.toString())
+            viewModel.photos.add(photo)
+        } else {
+            Log.e(ContentValues.TAG, "Image not saved. $uri")
+        }
+
+    }
+
+    fun hasCameraPermission() = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+    fun hasExternalStoragePermission() = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
     @Preview(showBackground = true)
     @Composable
--- app/src/main/java/com/findmyrecycling/MainViewModel.kt
+++ app/src/main/java/com/findmyrecycling/MainViewModel.kt
@@ -1,27 +1,35 @@
 package com.findmyrecycling
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.findmyrecycling.dto.Facility
+import com.findmyrecycling.dto.Photo
 import com.findmyrecycling.dto.Product
 import com.findmyrecycling.dto.User
 import com.findmyrecycling.service.ProductService
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
-    
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.launch
 import org.koin.core.component.getScopeId
 
 
 class MainViewModel(var productService: ProductService = ProductService()) : ViewModel() {
 
+    val photos: ArrayList<Photo> = ArrayList<Photo>()
+    internal val NEW_PRODUCT = "New Product"
     var products: MutableLiveData<List<Product>> = MutableLiveData<List<Product>>()
     var facility: MutableLiveData<List<Facility>> = MutableLiveData<List<Facility>>()
-
+    var selectedProdcut by mutableStateOf(Product())
     var user: User? = null
+
     private lateinit var firestore: FirebaseFirestore
+    private var storageReference = FirebaseStorage.getInstance().getReference()
 
 
     init {
