1307def327d4bd209a685277a5f981bcee0dc0c6
bentonjc <bentonjc@mail.uc.edu>
2022-04-19 14:01:39 -0400

    Moved Login button to the top of screen, moved photo functionality to profile page. Removed photo button from mainActivity

--- app/src/main/java/com/findmyrecycling/ProfileScreen.kt
+++ app/src/main/java/com/findmyrecycling/ProfileScreen.kt
@@ -1,9 +1,17 @@
 package com.findmyrecycling
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -14,13 +22,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import coil.compose.AsyncImage
 import com.findmyrecycling.dto.Facility
+import com.findmyrecycling.dto.Photo
 import com.findmyrecycling.ui.theme.FindMyRecyclingTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ProfileScreen : ComponentActivity() {
-    private val viewModel: MainViewModel by viewModel<MainViewModel>()
 
+    private val viewModel: MainViewModel by viewModel<MainViewModel>()
+    private var uri: Uri? = null
+    private lateinit var currentImagePath: String
+    private var strUri by mutableStateOf("")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -97,16 +115,92 @@ class ProfileScreen : ComponentActivity() {
                 }
                 Button(
                     onClick = {
-
+                        takePhoto()
                     }
                 )
                 {
-                    Text(text = "Camera")
+                    Text(text = "Photo")
                 }
+            }
+            AsyncImage(model = strUri, contentDescription = "Facility image")
+        }
+    }
+    private fun takePhoto() {
+        if (hasCameraPermission() == PackageManager.PERMISSION_GRANTED && hasExternalStoragePermission() == PackageManager.PERMISSION_GRANTED) {
+            // The user has already granted permission for these activities.  Toggle the camera!
+            invokeCamera()
+        } else {
+            // The user has not granted permissions, so we must request.
+            requestMultiplePermissionsLauncher.launch(
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+
+                    )
+            )
+        }
+    }
+    private val requestMultiplePermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { resultsMap ->
+        var permissionGranted = false
+        resultsMap.forEach {
+            if (it.value == true) {
+                permissionGranted = it.value
+            } else {
+                permissionGranted = false
+                return@forEach
+            }
+        }
+        if (permissionGranted) {
+            invokeCamera()
+        } else {
+            Toast.makeText(this, getString(R.string.cameraPermissionDenied), Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    private fun invokeCamera() {
+        val file = createImageFile()
+        try {
+            uri = FileProvider.getUriForFile(this, "com.findmyrecycling.fileprovider", file)
+        } catch (e: Exception) {
+            Log.e(ContentValues.TAG, "Error: ${e.message}")
+            var foo = e.message
+        }
+        getCameraImage.launch(uri)
+    }
+
+    private fun createImageFile(): File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "Facility_${timestamp}",
+            ".jpg",
+            imageDirectory
+        ).apply {
+            currentImagePath = absolutePath
+        }
+    }
+
+    private val getCameraImage =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                Log.i(ContentValues.TAG, "Image Location: $uri")
+                strUri = uri.toString()
+                val photo = Photo(localUri = uri.toString())
+                viewModel.photos.add(photo)
+            } else {
+                Log.e(ContentValues.TAG, "Image not saved. $uri")
             }
 
         }
-    }
+
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+    private fun hasExternalStoragePermission() =
+        ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+
 
     @Preview(showBackground = true)
     @Composable
