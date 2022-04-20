66c62c318b8143a95103fa06b810615d731ed081
Daniel Cullen <34354477+CullenDaniel@users.noreply.github.com>
2022-04-19 20:36:40 -0400

    Fixed Auto Complete Removed Hambuger

--- app/src/main/java/com/findmyrecycling/MainActivity.kt
+++ app/src/main/java/com/findmyrecycling/MainActivity.kt
@@ -27,16 +27,21 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.W800
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.PopupProperties
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import androidx.core.content.res.ResourcesCompat
@@ -69,19 +74,23 @@ class MainActivity : ComponentActivity() {
 
     var selectedProduct: Product? = null
     var selectedFacility: Facility? = null
-
+    private var strSelectedData: String =""
+    private var uri: Uri? = null
+    private lateinit var currentImagePath: String
     private var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
     private val viewModel: MainViewModel by viewModel<MainViewModel>()
     private var inProductName: String = ""
-
+    private var strUri by mutableStateOf("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val products = ArrayList<Product>()
-            products.add(Product(product = "Tin Can", productId = 0))
-            products.add(Product(product = "Car Door", productId = 1))
-            products.add(Product(product = "Glass", productId = 2))
+            viewModel.fetchProducts()
+            val products by viewModel.products.observeAsState(initial = emptyList())
+            //val products = ArrayList<Product>()
+            //products.add(Product(product = "Tin Can", productId = 0))
+            //products.add(Product(product = "Car Door", productId = 1))
+            //products.add(Product(product = "Glass", productId = 2))
             firebaseUser?.let {
                 val user = User(it.uid, "")
                 viewModel.user = user
@@ -94,15 +103,6 @@ class MainActivity : ComponentActivity() {
                 ) {
                     RecycleSearch("Android", products)
                 }
-            }
-
-            Button(
-                onClick = {
-                    signIn()
-                }
-            )
-            {
-                Text(text = "Log On")
             }
         }
     }
@@ -138,7 +138,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
+/*
     @Composable
     fun ProfileMenu() {
         Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd) {
@@ -146,24 +146,29 @@ class MainActivity : ComponentActivity() {
 
         }
     }
-
+*/
 
 
     @Composable
-    fun RecycleSearch(name: String, products: List<Product> = ArrayList<Product>(), selectedProduct: Product = Product()) {
+    fun RecycleSearch(name: String, products : List<Product> = ArrayList<Product>(), selectedProduct: Product = Product()) {
         var recyclable by remember (selectedProduct.productId){ mutableStateOf(selectedProduct.product) }
         var location by remember { mutableStateOf("") }
         val context = LocalContext.current
         Row {
             Arrangement.Center
-            ProfileMenu()
+            //ProfileMenu()
         }
 
         Column {
 
             ProductSpinner(products = products)
+            TextFieldWithDropdownUsage(dataIn = products,
+                "Recyclable",
+                3,
+                Product()
+            )
 
-            OutlinedTextField(
+            /*OutlinedTextField(
                 value = recyclable,
                 onValueChange = { recyclable = it },
                 label = { Text(stringResource(R.string.recyclable), fontSize = 17.sp, fontWeight = W800) },
@@ -176,7 +181,10 @@ class MainActivity : ComponentActivity() {
                         color = Color.Black,
                         shape = RoundedCornerShape(4.dp)
                     ),
+
             )
+
+             */
 
             OutlinedTextField(
                 value = location,
@@ -199,12 +207,19 @@ class MainActivity : ComponentActivity() {
                 Button(
                     onClick = {
                         viewModel.saveFacility()
-                        }
+                    }
                 )
                 {
                     Text(text = "Save", color = RecyclingBlue, fontSize = 17.sp)
                 }
-
+                Button(
+                    onClick = {
+                        takePhoto()
+                    }
+                )
+                {
+                    Text(text = "Photo", color = RecyclingBlue, fontSize = 17.sp)
+                }
                 Button(
                     onClick = {
                         addFacility()
@@ -213,8 +228,195 @@ class MainActivity : ComponentActivity() {
                 {
                     Text(text = "Add Facility", color = RecyclingBlue, fontSize = 17.sp)
                 }
+                Button(
+                    onClick = {
+                        signIn()
+                    }
+                )
+                {
+                    Text(text = "Log On", color = RecyclingBlue, fontSize = 17.sp)
+                }
+            }
+            AsyncImage(model = strUri, contentDescription = "Facility image")
+        }
+    }
+
+    //list of stuff added
+    @Composable
+    fun TextFieldWithDropdownUsage(dataIn: List<Product>, label : String = "", take :Int = 3, selectedProduct : Product = Product()) {
+
+        val dropDownOptions = remember { mutableStateOf(listOf<Product>()) }
+        val textFieldValue = remember(selectedProduct.productId) { mutableStateOf(TextFieldValue(selectedProduct.product)) }
+        val dropDownExpanded = remember { mutableStateOf(false) }
+
+        fun onDropdownDismissRequest() {
+            dropDownExpanded.value = false
+        }
+
+        fun onValueChanged(value: TextFieldValue) {
+            strSelectedData = value.text
+            dropDownExpanded.value = true
+            textFieldValue.value = value
+            dropDownOptions.value = dataIn.filter {
+                it.toString().startsWith(value.text) && it.toString() != value.text
+            }.take(take)
+        }
+
+        TextFieldWithDropdown(
+            modifier = Modifier.fillMaxWidth()
+
+                .padding(start = 4.dp, end = 4.dp, top = 2.dp, bottom = 2.dp)
+                .background(color = RecyclingGray)
+                .border(
+                    width = 1.dp,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(4.dp)
+                ),
+            value = textFieldValue.value,
+            setValue = ::onValueChanged,
+            onDismissRequest = ::onDropdownDismissRequest,
+            dropDownExpanded = dropDownExpanded.value,
+            list = dropDownOptions.value,
+            label = label
+        )
+    }
+    @Composable
+    fun TextFieldWithDropdown(
+        modifier: Modifier = Modifier,
+        value: TextFieldValue,
+        //fontSize: IntrinsicSize,
+        setValue: (TextFieldValue) -> Unit,
+        onDismissRequest: () -> Unit,
+        dropDownExpanded: Boolean,
+        list: List<Product>,
+        label: String = ""
+    ) {
+        Box(modifier) {
+            TextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onFocusChanged { focusState ->
+                        if (!focusState.isFocused)
+                            onDismissRequest()
+                    },
+                value = value,
+
+                onValueChange = setValue,
+                label = { Text(label) },
+                colors = TextFieldDefaults.outlinedTextFieldColors()
+
+            )
+            DropdownMenu(
+                expanded = dropDownExpanded,
+                properties = PopupProperties(
+                    focusable = false,
+                    dismissOnBackPress = true,
+                    dismissOnClickOutside = true
+                ),
+                onDismissRequest = onDismissRequest
+            ) {
+                list.forEach { text ->
+                    DropdownMenuItem(onClick = {
+                        setValue(
+                            TextFieldValue(
+                                text.toString(),
+                                TextRange(text.toString().length)
+                            )
+                        )
+                        selectedProduct = text
+                    }) {
+                        Text(text = text.toString())
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+    //end of stuff added
+    private fun takePhoto() {
+        if (hasCameraPermission() == PERMISSION_GRANTED && hasExternalStoragePermission() == PERMISSION_GRANTED) {
+            // The user has already granted permission for these activities.  Toggle the camera!
+            invokeCamera()
+        } else {
+            // The user has not granted permissions, so we must request.
+            requestMultiplePermissionsLauncher.launch(
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+
+                    )
+            )
+        }
+    }
+
+    private val requestMultiplePermissionsLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { resultsMap ->
+        var permissionGranted = false
+        resultsMap.forEach {
+            if (it.value == true) {
+                permissionGranted = it.value
+            } else {
+                permissionGranted = false
+                return@forEach
+            }
+        }
+        if (permissionGranted) {
+            invokeCamera()
+        } else {
+            Toast.makeText(this, getString(R.string.cameraPermissionDenied), Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    private fun invokeCamera() {
+        val file = createImageFile()
+        try {
+            uri = FileProvider.getUriForFile(this, "com.findmyrecycling.fileprovider", file)
+        } catch (e: Exception) {
+            Log.e(TAG, "Error: ${e.message}")
+            var foo = e.message
+        }
+        getCameraImage.launch(uri)
+    }
+
+    private fun createImageFile(): File {
+        val timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "Facility_${timestamp}",
+            ".jpg",
+            imageDirectory
+        ).apply {
+            currentImagePath = absolutePath
+        }
+    }
+
+    private val getCameraImage =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+            if (success) {
+                Log.i(ContentValues.TAG, "Image Location: $uri")
+                strUri = uri.toString()
+                val photo = Photo(localUri = uri.toString())
+                viewModel.photos.add(photo)
+            } else {
+                Log.e(ContentValues.TAG, "Image not saved. $uri")
             }
 
+        }
+
+    private fun hasCameraPermission() = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+    private fun hasExternalStoragePermission() =
+        ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        FindMyRecyclingTheme {
+            RecycleSearch("Android")
         }
     }
 
@@ -254,13 +456,5 @@ class MainActivity : ComponentActivity() {
     private fun addFacility() {
         val intent = Intent(this, ProfileScreen::class.java)
         startActivity(intent)
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        FindMyRecyclingTheme {
-            RecycleSearch("Android")
-        }
     }
 }
